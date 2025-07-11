d5c420ec62fb8d9cad775e0087bd883d674c0535
Maimai10808 <331686549@qq.com>
2025-07-11 14:58:39 +0800

    $ Fixed bug
    Fixed bug where segmented control did not show current interface style setting

--- ToDoListUIkit/ViewControllers/SettingsViewController.swift
+++ ToDoListUIkit/ViewControllers/SettingsViewController.swift
@@ -15,11 +15,31 @@ class SettingsViewController: UIViewController {
     
     @IBOutlet weak var modalVIew: UIView!
     
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         settingsTitleLabel.font = UIFont.style(.heading1)
         appthemeLabel.font = UIFont.style(.secondaryText)
+        
+        let window = UIApplication.shared
+            .connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .flatMap { $0.windows }
+            .first { $0.isKeyWindow }
+        if let window = window {
+            switch window.overrideUserInterfaceStyle {
+            case .light:
+                segmentedControl.selectedSegmentIndex = 0
+            case .dark:
+                segmentedControl.selectedSegmentIndex = 1
+            case .unspecified:
+                segmentedControl.selectedSegmentIndex = 2
+           @unknown default :
+                segmentedControl.selectedSegmentIndex = 2
+            }
+        }
     }
     
     override func viewDidLayoutSubviews() {
